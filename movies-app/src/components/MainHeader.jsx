1c2411bff90077d60dc2be613452911da9cca8d9
David Miculit <David.Miculit@cognizant.com>
2026-02-12 15:03:47 +0200

    feat/refactor: updated hero section and separated main header into its own file

--- movies-app/src/components/MainHeader.jsx
+++ movies-app/src/components/MainHeader.jsx
@@ -2,37 +2,37 @@ function MiddleHeaderButtons() {
   return (
     <div className="flex gap-8 items-center group">
       {["Watchlist", "Home", "Favorites"].map((item) => (
-        <h2
+        <nav
           key={item}
-          className="text-white cursor-pointer
+          className="text-white cursor-pointer text-lg
                      transition-all duration-200 ease-out
                      group-hover:opacity-60 group-hover:scale-100
                      hover:!opacity-100 hover:!scale-150"
         >
           {item}
-        </h2>
+        </nav>
       ))}
     </div>
   )
 }
 
-export default function MainHeader() {
-    return(
-      <header className="relative flex items-center gap-8 p-10">
-        <div className="flex-1" />
-        <MiddleHeaderButtons></MiddleHeaderButtons>
-        <form id="searchBar" className="flex flex-1 justify-end">
-          <input
-            id="searchInput"
-            type="text"
-            placeholder=""
-            autoComplete="off"
-            className="text-gray-300 w-full max-w-52 font-rubik rounded-md border border-slate-200 bg-slate-50 shadow-lg outline-none transition
-                     focus:border-gray-400 focus:ring-1 focus:ring-gray-400 p-0.5 bg-opacity-20"
-          >
-          </input>
-          <button type="submit" hidden></button>
-        </form>
-      </header>
-    )
+export default function MainHeader({name, description, imageUrl}) {
+  return(
+    <header className="relative flex items-center gap-8 p-10">
+      <div className="flex-1" />
+      <MiddleHeaderButtons></MiddleHeaderButtons>
+      <form id="searchBar" className="flex flex-1 justify-end">
+        <input
+          id="searchInput"
+          type="text"
+          placeholder=""
+          autoComplete="off"
+          className="text-gray-300 w-full max-w-52 font-rubik rounded-md border border-slate-200 bg-slate-50 shadow-lg outline-none transition
+                  focus:border-gray-400 focus:ring-1 focus:ring-gray-400 p-0.5 bg-opacity-20"
+        >
+        </input>
+        <button type="submit" hidden></button>
+      </form>
+    </header>
+  )
 }
