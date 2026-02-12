a9c51a69e09716fe0d1ec54a82946d9910ec2670
David Miculit <David.Miculit@cognizant.com>
2026-02-12 12:06:39 +0200

    fix: spinner moved to App.jsx and now working as intended

--- movies-app/src/App.jsx
+++ movies-app/src/App.jsx
@@ -30,18 +30,18 @@ function App() {
   }
 
   useEffect(() => { fetchMovies() }, [])
-    
-  if(isLoading) {
-    return <Spinner></Spinner>;
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-black">
+        <Spinner />
+      </div>
+    )
   }
 
   return (
     <div className="min-h-screen flex flex-col bg-black font-rubik"> 
-      <section className="relative h-[80vh] bg-cover bg-center" style={{backgroundImage:"url(https://i0.wp.com/bloody-disgusting.com/wp-content/uploads/2021/08/dune-poster-2-new.png?fit=1515%2C825&ssl=1)"}}>
-        <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-black/40 to-black" />
-
-        <MainHeader />
-      </section>
+      <MainHeader />
 
       <main className="relative bg-black">
         <div className="w-[90%] container mx-auto flex flex-col gap-10">
