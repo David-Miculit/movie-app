71b1a3977d07398f0d38ec7dfd054c906386d988
David Miculit <David.Miculit@cognizant.com>
2026-02-17 11:14:27 +0200

    refactor: use NavLink to show main/favorites page active state

--- movies-app/src/components/MainHeader.jsx
+++ movies-app/src/components/MainHeader.jsx
@@ -1,10 +1,7 @@
 import SearchBar from "./SearchBar"
-import { useNavigate, useLocation } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 function MiddleHeaderButtons() {
-  const navigate = useNavigate();
-  const location = useLocation()
-
   const items = [
     { label: "Watchlist", path: "/watchlist" },
     { label: "Home", path: "/" },
@@ -13,30 +10,27 @@ function MiddleHeaderButtons() {
 
   return (
     <div className="flex gap-6 items-center group">
-      {items.map(({ label, path }) => {
-        const isActive = location.pathname === path
+      {items.map(({ label, path }) =>
+        <NavLink
+          key={label}
+          to={path}
+          className={({isActive}) =>
+            `relative text-white cursor-pointer text-xl
+            transition-all duration-200 ease-out
+            group-hover:opacity-60 group-hover:scale-100
+            hover:!opacity-100 hover:!scale-125 max-[600px]:text-sm
 
-        return (
-          <nav
-            key={label}
-            onClick={() => navigate(path)}
-            className={
-              `relative text-white cursor-pointer text-xl
-              transition-all duration-200 ease-out
-              group-hover:opacity-60 group-hover:scale-100
-              hover:!opacity-100 hover:!scale-125 max-[600px]:text-sm
-
-              after:content-['']
-              after:absolute after:left-0 after:-bottom-1
-              after:h-[2px] after:bg-white
-              after:transition-all after:duration-300
-              ${isActive ? "after:w-full opacity-100" : "after:w-0"}`
-            }
-          >
-            {label}
-          </nav>
+            after:content-['']
+            after:absolute after:left-0 after:-bottom-1
+            after:h-[2px] after:bg-white
+            after:transition-all after:duration-300
+            ${isActive ? "after:w-full opacity-100" : "after:w-0"}`
+          }
+        >
+          {label}
+        </NavLink>
         )
-      })}
+      }
     </div>
   )
 }
