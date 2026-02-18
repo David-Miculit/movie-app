c7a8b74274ad440792de606cb62642dc54efa741
David Miculit <David.Miculit@cognizant.com>
2026-02-18 11:13:32 +0200

    fix: fetch data in MainLayout and pass as context to children

--- movies-app/src/layouts/MainLayout.jsx
+++ movies-app/src/layouts/MainLayout.jsx
@@ -1,19 +1,50 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MainHeader from "../components/MainHeader";
 import Footer from "../components/Footer";
+import Spinner from "../components/LoadingSpinner";
 
 export default function MainLayout() {
-    const [searchQuery, setSearchQuery] = useState("")
-    const isHome = useLocation().pathname=== "/"
+  const [searchQuery, setSearchQuery] = useState("")
+  const isHome = useLocation().pathname=== "/"
+  const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
-    return (
-        <div id={'main-layout'} className="min-h-screen bg-black flex flex-col">
-            <MainHeader searchQuery={searchQuery} setSearchQuery={setSearchQuery} className={isHome ? "absolute top-0 left-0 w-full z-50": ""}/>
-            <div className="flex-1">
-              <Outlet context={{searchQuery, setSearchQuery}} />
-            </div>
-            <Footer />
-        </div>
+  const fetchMovies = async() => {
+    try {
+      const response = await fetch("http://localhost:5000/api/movies")
+      if(!response.ok) {
+        throw new Error("Failed to fetch movies")
+      }
+
+      const data = await response.json()
+      if(data && data.length!=0) {
+        console.log(`Movie list data`, data)
+        setMovies(data)
+      } else {
+        console.log("No movie data")
+      }
+    } catch (err) {
+      console.log(err)
+      setMovies([])
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  useEffect(() => { fetchMovies() }, [])
+  if(isLoading) {
+    return(
+      <Spinner/>
     )
+  }
+
+  return (
+    <div id={'main-layout'} className="min-h-screen bg-black flex flex-col">
+        <MainHeader searchQuery={searchQuery} setSearchQuery={setSearchQuery} className={isHome ? "absolute top-0 left-0 w-full z-50": ""}/>
+        <div className="flex-1">
+          <Outlet context={movies}/>
+        </div>
+        <Footer />
+    </div>
+  )
 }
